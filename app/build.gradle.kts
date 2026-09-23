3cc5ddf3fcce0021f20732d36399579231e761a1
Rodolfo Hernández <rhernandez621@unab.edu.co>
2026-09-23 09:31:36 -0500

    maquetacion inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "me.rodolfohernandez.statecompose2"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
