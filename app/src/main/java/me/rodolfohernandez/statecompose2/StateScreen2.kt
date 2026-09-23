4047665d84c569c9032cf32e3b1ed4515c21bee1
Rodolfo Hernández <rhernandez621@unab.edu.co>
2026-09-23 09:32:57 -0500

    Contador funcional

--- app/src/main/java/me/rodolfohernandez/statecompose2/StateScreen2.kt
+++ app/src/main/java/me/rodolfohernandez/statecompose2/StateScreen2.kt
@@ -1,7 +1,6 @@
 package me.rodolfohernandez.statecompose2
 
-
-
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +9,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,14 +22,20 @@ import androidx.compose.ui.unit.sp
 @Preview(showSystemUi = true)
 @Composable
 fun StateScreen() {
+
+    var contador by remember { mutableStateOf(3) }
+
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxSize()
     ) {
-        Text(text = "Contador: 0", fontSize = 32.sp)
+        Text(text = "Contador: $contador", fontSize = 32.sp)
         Spacer(modifier = Modifier.height(15.dp))
-        Button(onClick = { }) {
+        Button(onClick = {
+            Log.i("mis-errores", "Estoy haciendo click!")
+            contador++
+        }) {
             Text("Sumar")
         }
     }
